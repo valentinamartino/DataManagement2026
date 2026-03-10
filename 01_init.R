4af295088e9e73ae35484b8f2b12879d24a4d4c1
Valentina Martino <valentina.martino@student.univaq.it>
2026-03-10 15:17:06 +0100

    Ciao

---
+++ 01_init.R
@@ -0,0 +1,13 @@
+# load packages
+library(learnr)  #carico il pacchetto learnr
+
+system('"C:/Program Files/Git/git-cmd.exe" --global user.name "valentinamartino"')
+system('"C:/Program Files/Git/git-cmd.exe" --global user.email "valentina.martino@student.univaq.it"')
+system('git --global ')
+system('Omit --global')
+"C:\Program Files\Git\git-cmd.exe"
+install.packages(c("usethis","gitcreds"))
+
+gitcreds::gitcreds_set()
+3
+system('git --help')
