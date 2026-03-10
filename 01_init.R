718fcfa4e20f222d2bc4bc942081da44f770a98a
Valentina Martino <valentina.martino@student.univaq.it>
2026-03-10 15:55:46 +0100

    second commit

--- 01_init.R
+++ 01_init.R
@@ -1,13 +1,23 @@
 # load packages
 library(learnr)  #carico il pacchetto learnr
 
-system('"C:/Program Files/Git/git-cmd.exe" --global user.name "valentinamartino"')
-system('"C:/Program Files/Git/git-cmd.exe" --global user.email "valentina.martino@student.univaq.it"')
+system('git config --global user.name "valentinamartino"')
+system('git config --global user.email "valentina.martino@student.univaq.it"')
 system('git --global ')
-system('Omit --global')
+system('git config --local -l')
 "C:\Program Files\Git\git-cmd.exe"
 install.packages(c("usethis","gitcreds"))
-
+system('git config --global user.name "valentinamartino"')
+system('git config --global user.email "valentina.martino@student.univaq.it"')
 gitcreds::gitcreds_set()
 3
 system('git --help')
+
+system('git config --global user.name "Valentina Martino"')
+system('git config --global user.email "valentina.martino@student.univaq.it"')
+system('git config --global --list')
+system('git remote add origin https://github.com/valentinamartino/DataManagement2026.git')
+
+gitcreds::gitcreds_set()
+
+usethis::use_git_remote(name = "origin", url = 'https://github.com/valentinamartino/DataManagement2026.git')
